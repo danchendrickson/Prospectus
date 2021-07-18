691214163a4e10d7a24605bad1986b634a39509f
danchendrickson <dchendrickson01@email.wm.edu>
2021-07-18 13:00:38 -0400

    Update first run

--- pi_first_run.sh
+++ pi_first_run.sh
@@ -2,9 +2,12 @@ sudo apt-get update
 sudo apt full-upgrade -y
 sudo apt-get install build-essential python-dev python-pip -y
 sudo apt-get install code -y
+sudo apt-get install python-smbus -y
+
 sudo pip install smbus2 i2c-tools -y
 sudo pip install py-imu-mpu6050 -y
+sudo pip install numpy -y
+sudo pip install matplotlib -y
+sudo pip install RPi.GPIO -y
 
 sudo raspi-config
-sudo apt-get install python-smbus -y
-sudo pip install RPi.GPIO -y
